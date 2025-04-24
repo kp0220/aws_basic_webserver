97d283623c1f60bc06e93b50a8d55a300cbd2900
Kavit Patel <kavit1986@gmail.com>
2025-04-25 00:13:04 +0530

    Added SSH command and private key path outputs, updated AMI ID

--- output.tf
+++ output.tf
@@ -5,3 +5,12 @@ output "instance_ip" {
 output "website_url" {
   value = "http://${aws_instance.web.public_dns}"
 }
+
+output "ssh_command" {
+  value = "ssh -i terraform-key.pem ec2-user@${aws_instance.web.public_ip}"
+}
+
+output "private_key_path" {
+  description = "Path to the generated private key file"
+  value       = local_file.private_key.filename
+}
--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "aws_region" {
 
 variable "ami_id" {
   description = "AMI ID for the instance"
-  default     = "ami-0c02fb55956c7d316"  # Amazon Linux 2 AMI (for example)
+  default     = "ami-0e449927258d45bc4"  # Amazon Linux 2 AMI (for example)
 }
 
 variable "instance_type" {
